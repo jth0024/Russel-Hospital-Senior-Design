7897cf8711576a9e4a0cb722a23efe8c7c5b5419
joshstarling92 <joshstarling92@gmail.com>
2014-09-21 14:24:49 -0500

    SQL database query update
    All three methods of querying the database are functioning. The three
    methods are to get back: the entire table, a row of values, and a singe
    column point.

--- server-files/sql_insert.py
+++ server-files/sql_insert.py
@@ -4,11 +4,11 @@ from sql_query import queryRow
  
 from sqlalchemy_declarative import Base, Devices
  
+
 engine = create_engine('sqlite:///rh.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
- 
 DBSession = sessionmaker(bind=engine)
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
@@ -18,49 +18,11 @@ DBSession = sessionmaker(bind=engine)
 # revert all of them back to the last commit by calling
 # session.rollback()
 session = DBSession()
- 
-# Insert a Person in the person table
-# new_device1 = Devices(name='device 1', ini='controller1', ip='192.168.92.65', mac='poop')
-# new_device2 = Devices(name='device 2', ini='controller2', ip='192.168.92.67', mac='poop')
-# session.add(new_device2,new_device1)
-# session.commit()
 
-
-# populate = ['device3','controller3','192.168.92.44','hello Im a mac']
-# #{'ini': 'controller2', 'ip': '192.168.92.67', 'mac': 'poop', 'id': '1', 'name': 'device 2'}
-
-# i = insert(table)
-# i = i.values({'name': 'device 3','ini': 'controller3', 'ip': '192.168.92.67', 'mac': 'poop'})
-# session.execute(i)
-# session.commit()
-
-def insertNewRow(table, dictKeys,*args):
+#Functions takes the the table name(i.e. the class found in sql_declarative) and a dictionary to insert all of the values into the database
+def insertNewRow(table, TheDict):
 	l = insert(table)
-	TheDict = {}
-	keytupletemp = tuple(dictKeys)
-	for i in range(0,len(args)):
-		print i
-		keytuple = keytupletemp[i]
-		value = args[i]
-		TheDict[keytuple] = value
-		print TheDict
+	l = l.values(TheDict)
 	session.execute(l)
 	session.commit()
 
-table = Devices
-deviceDict = queryRow(table)
-print deviceDict
-insertNewRow(table,deviceDict.keys(),'deviceJosh','controllerJosh','192.168.92.Josh','hello Im a Josh')
-results = queryRow(table)
-print results
-
-
-
-	# def userInput(ItemA, ItemB, *args):    
- #    lst=[]
- #    lst.append(ItemA)
- #    lst.append(ItemB)
- #    for arg in args:
- #        lst.append(arg)
-
- #    print ' '.join(lst)
--- server-files/sql_query.py
+++ server-files/sql_query.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
- 
+import sqlite3
 from sqlalchemy_declarative import Base, Devices
  
 def row2dict(row):
@@ -9,8 +9,16 @@ def row2dict(row):
         d[column.name] = str(getattr(row, column.name))
     return d
 
+def list2dict(list):
+	d = {}
+	for i in range(0, len(list)):
+		key = list[i][0]
+		d[key]= list[i]
+	return d
+
 def queryRow(tableName):
-	#SQL alchemy procesc for creating the engine and session to query the database
+	#SQL alchemy process for creating the engine and session to query the database
+	#insert table name as a variable not a string
 	engine = create_engine('sqlite:///rh.db')
 	Base.metadata.bind = engine
 	DBSession = sessionmaker()
@@ -22,6 +30,26 @@ def queryRow(tableName):
 	#query is then converted into a dictionary in which the database's column name is the key and the value is value.
 	dict = row2dict(databaseRow)
 	return dict
-table = Devices
-deviceDict = queryRow(table)
-print deviceDict
+
+def queryTable(tableName):
+	#Requires table name as a string because it works using SQLite3. Method will return a dictionary of the entire table.
+	#The keys are the unique ID,but can be changed via the list2dict function.(line 15)
+	db = sqlite3.connect('rh.db')
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM "+tableName)
+	fetchedValue = cursor.fetchall()
+	print fetchedValue
+	tableDictionary = list2dict(fetchedValue)
+	return tableDictionary
+
+
+
+def queryValue(tableName,columnName):
+	#Note: all inputs must be entered in as string. This comes from the fact that it was done through
+	#SQLite3 instead of SQL_alchemy
+	db = sqlite3.connect('rh.db')
+	cursor = db.cursor()
+	cursor.execute("SELECT " +columnName+ " FROM "+tableName+" ORDER BY id DESC")
+	fetchedValue = cursor.fetchone()
+	return fetchedValue
+	db.close()
