737605c197959fa5364bf286e6e214bbf3ea6da5
Adam Blood <adamlblood@gmail.com>
2014-10-13 13:17:02 -0500

    Changed the Create Device Chain file to use a dictionary key to create the nodes in the linked list

--- controller-files/createDeviceChain.py
+++ controller-files/createDeviceChain.py
@@ -3,22 +3,34 @@ sys.path.append('../database/database')
 from iniParser import iniParser, CompIPToRequestIP
 from Class import Device
 from sql_query import queryColumn 
-from sql_declarative import Devices
+#from sql_declarative import Devices
 
 def createChain():
     iniName = queryColumn('Devices', "ini")
     for x in range(0,len(iniName)):
         value = iniParser("iniFiles/" + str(iniName[x]))
-        value.append(CompIPToRequestIP(value[1]))
+        value['requestAddress'] = str(CompIPToRequestIP(value[1]))
         if x == 0:
-            deviceList = Device(value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9])
+            deviceList = Device(value['objectName'],value['address'],
+            value['objectIdentifier'],value['maxApduLengthAccepted'],
+            value['segmentationSupported'],value['vendorIdentifier'],
+            value['foreignPort'],value['foreignBBMD'],
+            value['foreignTTL'],value['requestAddress'])
             counter = deviceList
         elif x ==1 :
-            temp = Device(value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9])
+            temp = Device(value['objectName'],value['address'],
+            value['objectIdentifier'],value['maxApduLengthAccepted'],
+            value['segmentationSupported'],value['vendorIdentifier'],
+            value['foreignPort'],value['foreignBBMD'],
+            value['foreignTTL'],value['requestAddress'])
             deviceList.addDevice(temp)
             counter = counter.getNext()
         else:
-            temp = Device(value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9])
+            temp = Device(value['objectName'],value['address'],
+            value['objectIdentifier'],value['maxApduLengthAccepted'],
+            value['segmentationSupported'],value['vendorIdentifier'],
+            value['foreignPort'],value['foreignBBMD'],
+            value['foreignTTL'],value['requestAddress'])
             counter.addDevice(temp)
             counter = counter.getNext()
             
