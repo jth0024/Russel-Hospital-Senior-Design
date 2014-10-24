4a4953c91462bd0a0fe5215ecf44292c1b362cf0
adamlblood <adamlblood@gmail.com>
2014-10-24 00:32:42 -0500

    Revert "Eclipse Files"
    This reverts commit 9e0c7c5846578ef3bacfa62e02588925ec13a1b1.

--- RussellHospital/AirHandlerControllRev2/database/database/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sql_declarative import databaseCreation
-from .sql_insert import insertNewRow
-from .sql_query import queryRow, queryRowSpecific, queryColumn, queryTable,queryValue, queryValueSpecific
