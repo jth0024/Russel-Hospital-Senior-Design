82938b98e34b8535a508461f313e5b8a821400d2
Adam Blood <adamlblood@gmail.com>
2014-12-03 21:39:32 -0600

    Added Comments to createDeviceChain.py

--- controller-files/createDeviceChain.py
+++ controller-files/createDeviceChain.py
@@ -1,3 +1,7 @@
+# This file creates a singly linked list of Device objects. Each device object holds all of the information that is needed for a single controller and a pointer to the next device object in the list. 
+#The last item in the list holds a pointer to a null item to signify the end of the list.
+
+
 import sys
 sys.path.append('../database/database')
 from iniParser import iniParser, CompIPToRequestIP, parsePorts
@@ -5,40 +9,46 @@ from Class import *
 from sql_query import queryColumn 
 
 
-#from sql_declarative import Devices
 
-
+#SUMMARY
 def createChain():
-    iniName = queryColumn('devices', "ini", 'sqlite:///../database/database/rh.db')
+    # This retrives the name of all of the ini files that are listed in the database.
+    iniName = queryColumn('devices', "ini")
+        
     for index in range(0,len(iniName)):
-        portDic = None
-        deviceDic = None
+        portDic = None  #a dictionary of port objects
+        deviceDic = None #a dictionary of device objects
+        
+        #ADD A CHECK TO SEE IF FILE EXISTS
+        
+        #parses the ini file section, BACPYPES, to return a dictionary of controller properties
         deviceDic = iniParser("iniFiles/" + str(iniName[index]))
+        #adds the request IP to the dictionary (this is the device IP - 1)
         deviceDic['requestip'] = CompIPToRequestIP(deviceDic['address'])
+        #parses the PORTS section of the ini file to return a dictionary of port properties
         portDic = parsePorts("iniFiles/" + str(iniName[index]))
 
+        #for each item in the ports dictionary, create a port object of that name and return it to a dictionary with the port number as the dictionary key
         for port in portDic:
             portDic[port] = instantiatePortClass(portDic[port], portNumber(port))
-
+        
+        #This if statement links the device objects into the linked list. I do not think the elif statement is nessacary but I did not want to remove it or change the cose befor the presentation.
         if index == 0:
             deviceList = Device(deviceDic, portDic)
             counter = deviceList
         elif index ==1 :
             temp = Device(deviceDic, portDic)
-
             deviceList.addDevice(temp)
-            counter = counter.getNext()    
-                   
+            counter = counter.getNext()                     
         else:
             temp = Device(deviceDic, portDic)
             counter.addDevice(temp)
             counter = counter.getNext()
 
-
     return deviceList
 
 
-
+#This method 
 def instantiatePortClass(str_className,portNum):
     if str_className.lower() == "led":
         return DamperPositionOA(portNum)
@@ -49,7 +59,7 @@ def instantiatePortClass(str_className,portNum):
     else:
         return "Error: Type not reconised"
     
-    
+#   
 def portNumber(str_portNum):
     if str_portNum.lower() == "portone":
         return 1
